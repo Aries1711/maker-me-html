7be171bd350f37fae83074e27c1f6a70d136cd04
Aries1711 <37827800+Aries1711@users.noreply.github.com>
2021-08-23 20:43:35 +0700

    slider maker carousel

--- public/my-js/my-main.js
+++ public/my-js/my-main.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
     //owl-carousel home maker-slider
     $('.home .site-main .maker-slider .owl-carousel').owlCarousel({
-    	loop: true,
-    	autoplay: true,
     	dots:true,
+		loop: true,
+		autoplay: false,
     	responsive:{
     		0:{
     			items:1
     		},
     		544:{
-    			items:2
+    			items:1
     		}
     	}
     })
